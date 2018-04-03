64b745eec2d47cd725186c7a0bd5ed5866e72033
Phoenix <phoenix214@protonmail.com>
2018-04-03 20:42:11 +0500

    Updates on 1.4 - added authorization for geolocator

--- BreatheKlere/BreatheKlerePage.xaml.cs
+++ BreatheKlere/BreatheKlerePage.xaml.cs
@@ -5,6 +5,9 @@ using Xamarin.Forms;
 using Xamarin.Forms.GoogleMaps;
 using BreatheKlere.REST;
 using Plugin.Geolocator;
+using System.Diagnostics;
+using Plugin.Permissions;
+using Plugin.Permissions.Abstractions;
 
 namespace BreatheKlere
 {
@@ -131,10 +134,52 @@ namespace BreatheKlere
 		async protected override void OnAppearing()
 		{
             base.OnAppearing();
-            var locator = CrossGeolocator.Current;
-            var pos = await locator.GetPositionAsync(TimeSpan.FromTicks(10000));
-            Position position = new Position(pos.Latitude, pos.Longitude);
-            map.MoveToRegion(MapSpan.FromCenterAndRadius(position, Distance.FromMeters(5000)));
+            try
+            {
+                var status = await CrossPermissions.Current.CheckPermissionStatusAsync(Permission.Location);
+                if (status != PermissionStatus.Granted)
+                {
+                    if (await CrossPermissions.Current.ShouldShowRequestPermissionRationaleAsync(Permission.Location))
+                    {
+                        await DisplayAlert("Need location", "Gunna need that location", "OK");
+                    }
+
+                    var results = await CrossPermissions.Current.RequestPermissionsAsync(Permission.Location);
+                    //Best practice to always check that the key exists
+                    if (results.ContainsKey(Permission.Location))
+                        status = results[Permission.Location];
+                }
+
+                if (status == PermissionStatus.Granted)
+                {
+                    if (IsLocationAvailable())
+                    {
+                        var locator = CrossGeolocator.Current;
+                        var pos = await locator.GetPositionAsync(TimeSpan.FromTicks(10000));
+                        Position position = new Position(pos.Latitude, pos.Longitude);
+                        map.MoveToRegion(MapSpan.FromCenterAndRadius(position, Distance.FromMeters(5000)));
+                    }
+
+                }
+                else if (status != PermissionStatus.Unknown)
+                {
+                    await DisplayAlert("Location Denied", "Can not continue, try again.", "OK");
+                }
+            }
+            catch (Exception ex)
+            {
+
+                await DisplayAlert("Error: ",ex.Message,"OK");
+            }
+
 		}
+
+        public bool IsLocationAvailable()
+        {
+            if (!CrossGeolocator.IsSupported)
+                return false;
+
+            return CrossGeolocator.Current.IsGeolocationAvailable;
+        }
 	}
 }
