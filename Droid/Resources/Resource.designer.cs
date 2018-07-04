d83fca46c50cb22acc16d6ed9e741f3689d08ace
Phoenix <phoenix214@protonmail.com>
2018-07-05 01:03:52 +0500

    Fixed the reverse button issue

--- Droid/Resources/Resource.designer.cs
+++ Droid/Resources/Resource.designer.cs
@@ -2327,26 +2327,26 @@ namespace BreatheKlere.Droid
 			// aapt resource value: 0x7f020053
 			public const int avd_hide_password = 2130837587;
 			
-			// aapt resource value: 0x7f020127
-			public const int avd_hide_password_1 = 2130837799;
-			
 			// aapt resource value: 0x7f020128
-			public const int avd_hide_password_2 = 2130837800;
+			public const int avd_hide_password_1 = 2130837800;
 			
 			// aapt resource value: 0x7f020129
-			public const int avd_hide_password_3 = 2130837801;
+			public const int avd_hide_password_2 = 2130837801;
+			
+			// aapt resource value: 0x7f02012a
+			public const int avd_hide_password_3 = 2130837802;
 			
 			// aapt resource value: 0x7f020054
 			public const int avd_show_password = 2130837588;
 			
-			// aapt resource value: 0x7f02012a
-			public const int avd_show_password_1 = 2130837802;
-			
 			// aapt resource value: 0x7f02012b
-			public const int avd_show_password_2 = 2130837803;
+			public const int avd_show_password_1 = 2130837803;
 			
 			// aapt resource value: 0x7f02012c
-			public const int avd_show_password_3 = 2130837804;
+			public const int avd_show_password_2 = 2130837804;
+			
+			// aapt resource value: 0x7f02012d
+			public const int avd_show_password_3 = 2130837805;
 			
 			// aapt resource value: 0x7f020055
 			public const int bicycling = 2130837589;
@@ -2960,11 +2960,11 @@ namespace BreatheKlere.Droid
 			// aapt resource value: 0x7f020120
 			public const int notification_icon_background = 2130837792;
 			
-			// aapt resource value: 0x7f020125
-			public const int notification_template_icon_bg = 2130837797;
-			
 			// aapt resource value: 0x7f020126
-			public const int notification_template_icon_low_bg = 2130837798;
+			public const int notification_template_icon_bg = 2130837798;
+			
+			// aapt resource value: 0x7f020127
+			public const int notification_template_icon_low_bg = 2130837799;
 			
 			// aapt resource value: 0x7f020121
 			public const int notification_tile_bg = 2130837793;
@@ -2973,10 +2973,13 @@ namespace BreatheKlere.Droid
 			public const int notify_panel_notification_icon_bg = 2130837794;
 			
 			// aapt resource value: 0x7f020123
-			public const int splash_screen = 2130837795;
+			public const int reverse = 2130837795;
 			
 			// aapt resource value: 0x7f020124
-			public const int walking = 2130837796;
+			public const int splash_screen = 2130837796;
+			
+			// aapt resource value: 0x7f020125
+			public const int walking = 2130837797;
 			
 			static Drawable()
 			{
